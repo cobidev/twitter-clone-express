29fb9d22d99a1eb6ca35df55b11f573ec121d2de
Jacobo Martinez <jacobojavier98@gmail.com>
2019-07-20 18:23:14 -0400

    Fix CleanWebPackPlugin is not a constructor error

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
@@ -11,15 +11,12 @@ module.exports = {
     path: path.resolve(__dirname, '/public')
   },
   optimization: {
-    minimizer: [
-      new OptimizeCssAssetsPlugin(),
-      new TerserPlugin(),
-    ]
+    minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()]
   },
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: 'bundle.css',
+      filename: 'bundle.css'
     })
   ],
   module: {
@@ -34,8 +31,9 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader, //3. Extract css into files
           'css-loader', //2. Turns css into commonjs
-          'sass-loader'] //1. Turns sass into css
-      },
+          'sass-loader'
+        ] //1. Turns sass into css
+      }
     ]
   }
-}
+};
